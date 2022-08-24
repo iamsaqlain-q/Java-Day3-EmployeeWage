5521e36f3a65de90f5b7a15b3bfede03527fb7ce
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-24 12:37:45 +0530

    Displaying Welcome Message To Employee Wage Computation Program

---
+++ EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.bl.day3;
+
+public class EmployeeWage {
+	
+	public static void main(String[] args) {
+		
+		//Master-Branch
+				System.out.println("Welcome to Employee Wage Computation Program :)");
+
+	}
+}
