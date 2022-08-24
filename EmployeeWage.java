e3f9091dc1e3a0a3e403e2aad4f0edfe57b04d40
iamsaqlain_q <iamkhatik786@gmail.com>
2022-08-24 13:25:43 +0530

    Check Employee is

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,11 +1,20 @@
 package com.bl.day3;
+import java.util.Random;
 
 public class EmployeeWage {
 	
 	public static void main(String[] args) {
 		
 		//Master-Branch
-				System.out.println("Welcome to Employee Wage Computation Program :)");
-
+		System.out.println("Welcome to Employee Wage Computation Program :)");
+				
+		//UC-1-Check Employee is Present or Absent
+		Random empCheck = new Random();
+     int n = empCheck.nextInt(2);
+     if (n == 1)
+		System.out.println("Employee is Present");
+     else
+		System.out.println("Employee is Absent");
+		        
 	}
 }
